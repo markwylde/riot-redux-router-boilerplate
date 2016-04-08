6aee9ea90cb227b93bc83777dc217b33f49cc48c
Mark Wylde <mark.wylde@verl.com>
2016-04-08 22:23:22 +0100

    Added build distribution

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import riot from 'riot'
 
 import './tags/app.tag!'
 
+const body = document.createElement('body')
+body.innerHTML = '<app></app>'
+
+document.body = body
+            
 riot.mount('*')
